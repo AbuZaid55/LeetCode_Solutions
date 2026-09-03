d9d11ff0b845d07f36f8f247df5c2f7e0daa288a
Abu Zaid <zaid70979@gmail.com>
2026-09-04 00:03:29 +0530

    Time: 0 ms (100%), Space: 10.4 MB (84.77%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void getSubsets(vector<int>& nums, int start,
+                    vector<int>& current,
+                    vector<vector<int>>& result) {
+
+        result.push_back(current);
+
+        for (int i = start; i < nums.size(); i++) {
+
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            current.push_back(nums[i]);
+
+            getSubsets(nums, i + 1, current, result);
+
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+
+        sort(nums.begin(), nums.end());
+
+        getSubsets(nums, 0, current, result);
+
+        return result;
+    }
+};
