4f4a3f4e09aa22bd4b61c81d0f3b1e5dae5cee38
Abu Zaid <zaid70979@gmail.com>
2026-09-13 09:08:38 +0530

    Time: 3 ms (26.95%), Space: 9.9 MB (82.38%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    void makeSubset(vector<int>& current, int start, vector<int>& nums, vector<vector<int>>& result){
-        result.push_back(current);
+    // void makeSubset(vector<int>& current, int start, vector<int>& nums, vector<vector<int>>& result){
+    //     result.push_back(current);
 
-        for(int i = start; i<nums.size(); i++){
-            current.push_back(nums[i]);
-            makeSubset(current, i + 1, nums, result );
-            current.pop_back();
-        }
-    }
+    //     for(int i = start; i<nums.size(); i++){
+    //         current.push_back(nums[i]);
+    //         makeSubset(current, i + 1, nums, result );
+    //         current.pop_back();
+    //     }
+    // }
     vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> result;
+        // vector<int> current;
+        // makeSubset(current, 0, nums, result);
+        // return result;
+
         vector<vector<int>> result;
-        vector<int> current;
-        makeSubset(current, 0, nums, result);
+
+        int n = nums.size();
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+
+            vector<int> current;
+
+            for (int i = 0; i < n; i++) {
+
+                if (mask & (1 << i)) {
+                    current.push_back(nums[i]);
+                }
+            }
+
+            result.push_back(current);
+        }
+
         return result;
     }
 };
